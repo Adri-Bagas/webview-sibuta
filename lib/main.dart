eae34ab2321b18a8456557deab797f8a8674b983
Mahesa Alfian Dhika <mahesaalfian03@gmail.com>
2024-03-15 16:34:30 +0700

    ganti package ke flutter_webview_pro agar support file upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,33 +24,65 @@ class MyApp extends StatelessWidget {
 }
 
 class WebViewMain extends StatefulWidget {
-  const WebViewMain({super.key});
+  const WebViewMain({Key? key});
 
   @override
   State<WebViewMain> createState() => _WebViewMainState();
 }
 
 class _WebViewMainState extends State<WebViewMain> {
-
-  var _controller;
+  late final Completer<WebViewController> _controllerCompleter;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..enableZoom(false)
-      ..loadRequest(Uri.parse('https://sibuta.smktarunabhakti.net:3996'));
+    _controllerCompleter = Completer<WebViewController>();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: WebViewWidget(controller: _controller)),
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://sibuta.smktarunabhakti.net:3996',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controllerCompleter.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            print("WebView is loading (progress : $progress%)");
+          },
+          javascriptChannels: <JavascriptChannel>{
+            _toasterJavascriptChannel(context),
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              print('blocking navigation to $request');
+              return NavigationDecision.prevent;
+            }
+            print('allowing navigation to $request');
+            return NavigationDecision.navigate;
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+          geolocationEnabled: false, 
+        ),
+      ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+      name: 'toaster',
+      onMessageReceived: (JavascriptMessage message) {
+        print('Message received: ${message.message}');
+      },
     );
   }
 }
-
